14de1457da9ff36e7aeab15073bd3b442df8309f
nvkuznetsova <nvkuznetsova@list.ru>
2019-01-17 19:09:42 +0300

    update

---
+++ client/build/precache-manifest.e5424c21a88ce784d15d3c2b793ab170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d645c3df2d6a207077c",
+    "url": "/static/js/main.8d645c3d.chunk.js"
+  },
+  {
+    "revision": "607971345518254aa2c8",
+    "url": "/static/js/1.60797134.chunk.js"
+  },
+  {
+    "revision": "8d645c3df2d6a207077c",
+    "url": "/static/css/main.af047c69.chunk.css"
+  },
+  {
+    "revision": "dc47482cbee46c59b3c653ad6ea61f50",
+    "url": "/index.html"
+  }
+];
